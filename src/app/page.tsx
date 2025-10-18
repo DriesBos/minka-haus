a1c639c631a9cf0adf31f452e7f2ab00413ad5f5
DriesBos <info@driesbos.com>
2025-10-18 09:14:32 +0900

    splash page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import Image from 'next/image';
+'use client';
+
+import { useEffect, useState } from 'react';
 import styles from './page.module.sass';
 
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles.description}>
+        <div
+          className={`${styles.description} ${isLoaded ? styles.fadeIn : ''}`}
+        >
           <p>A place for downtime, creation and connection</p>
         </div>
       </main>
