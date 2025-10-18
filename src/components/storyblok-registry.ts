943786b983fcbed78c821aadd705e3baa24e6b06
DriesBos <info@driesbos.com>
2025-10-18 18:07:04 +0900

    deploy fix attempt

--- src/components/storyblok-registry.ts
+++ src/components/storyblok-registry.ts
@@ -1,10 +1,10 @@
 // This file exists to avoid the casing conflicts with direct imports
-import PageComponent from './storyblok/page';
-import PageHomeComponent from './storyblok/page-home/page-home';
-import TeaserComponent from './storyblok/teaser';
+import Page from './storyblok/page';
+import PageHome from './storyblok/page-home/page-home';
+import Teaser from './storyblok/teaser';
 
 export const storyblokComponents = {
-  page: PageComponent,
-  'page-home': PageHomeComponent,
-  teaser: TeaserComponent,
+  page: Page,
+  'page-home': PageHome,
+  teaser: Teaser,
 };
