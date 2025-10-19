242ede5ab6226ed82d122f7d436d961f080731cd
DriesBos <info@driesbos.com>
2025-10-19 14:47:02 +0900

    fixing deploy case insensitivity

--- src/components/storyblok-registry.ts
+++ src/components/storyblok-registry.ts
@@ -1,10 +1,8 @@
 // This file exists to avoid the casing conflicts with direct imports
-import Page from '@/components/storyblok/page';
-import PageHome from '@/components/storyblok/page-home/page-home';
-import Teaser from '@/components/storyblok/teaser';
+import PageGeneral from '@/components/storyblok/PageGeneral/PageGeneral';
+import PageHome from '@/components/storyblok/PageHome/PageHome';
 
 export const storyblokComponents = {
-  page: Page,
-  'page-home': PageHome,
-  teaser: Teaser,
+  PageGeneral: PageGeneral,
+  PageHome: PageHome,
 };
